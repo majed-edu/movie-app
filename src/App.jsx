bbda9564ab17e5d9f5078daaf33110dd52fe536f
ماجد حسين صالح قايد الحدي <23160077@su.edu.ye>
2026-09-15 04:37:48 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h2>Find <span className="text-gradient">Great Movies</span> You'll Enjoy Without the Hassle</h2>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
